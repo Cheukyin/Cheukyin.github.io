8f6f2a026f5a7db4a75f352d26747640159580b5
Cheukyin Yip <cheukyinyip0077@gmail.com>
2014-08-06 18:07:40 +0800

    Use pygments lib directly instead of call pygmentize externally in _pygment-html.py

--- _pygment-html.py
+++ _pygment-html.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.7
 #coding=utf-8
-import os
 from bs4 import BeautifulSoup
 import sys
 import re
@@ -40,12 +39,6 @@ class Pygments_Html:
             # code_segment exported by org-mode inside <div class="org-src-container">
             src_soup = soup.find("div",class_="org-src-container")
 
-            # write src_segment to file "temp"
-            src_segment = src_soup.text
-            temp=open('temp','w')
-            temp.write(src_segment)
-            temp.close()
-
             # code_segment exported by org-mode has a tag <pre class="src src-lang">,
             # lang identifies the code language
             language = (src_soup.pre['class'][1]).split('-')[1]
@@ -56,14 +49,14 @@ class Pygments_Html:
                 language = self.language_dict['others']
             print "Colorizing " + language
 
+            lexer = get_lexer_by_name(language, stripall=True)
+            formatter = HtmlFormatter(linenos='inline', cssclass="highlight")
+
             # Colorize the code with pygmentize
-            src_colorized = os.popen('pygmentize -f html -O linenos=inline -l ' + language + ' temp').read()
+            src_colorized = highlight(src_soup.text, lexer, formatter)
 
             # replace src_html with src_colorized in file_read
             file_read = file_read.replace(src_html,src_colorized)
-
-        #remove file "temp"
-        os.remove('temp')
 
         file = open( self.filename,'w' )
         file.write(file_read)
